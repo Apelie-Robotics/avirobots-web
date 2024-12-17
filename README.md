373c25484b568f221b5bc4fa4a821f3e6fd7432c
Francisco Cáceres <francaceres98@gmail.com>
2024-12-17 13:15:21 -0300

    feat(TrialForm): trial form fully functional

--- README.md
+++ README.md
@@ -1,20 +1,24 @@
 # Avisense website
+
 This project was developed in may 2024 to serve as the website for the project Avisense by Apelie Robotics.
 
 ## Technologies used
-* React
-* SCSS
-* EmailJs (allowing the form in the contact page to send emails to a determined email address)
+
+- React
+- SCSS
+- Formspree
 
 ## Libraries used
-* React Spring for the mobile navbar animation
-* React Icons
+
+- React Icons
 
 ## How to run
+
 To run the project, you should:
+
 1. git clone the repository in your pc
 2. run npm install to install all the packages used
 3. run npm run dev to create the local host
 4. run sass --watch src/scss/main.scss src/App.css to make sure the changes made in the scss files are compiled to the app styles file. Sass must be previously installed.
 
-About SCSS: the .scss files are all located in the /src/scss folder and divided according to their uses. Each file is then imported in the main.scss file, which is the one the sass compiler uses to write in the App.css file, then imported in the App.jsx file as its styles 
+About SCSS: the .scss files are all located in the /src/scss folder and divided according to their uses. Each file is then imported in the main.scss file, which is the one the sass compiler uses to write in the App.css file, then imported in the App.jsx file as its styles
--- src/components/TrialForm/index.jsx
+++ src/components/TrialForm/index.jsx
@@ -1,60 +1,158 @@
+import { useForm, ValidationError } from "@formspree/react";
+import { useEffect, useState } from "react";
+
 const TrialForm = () => {
+  const [state, handleSubmit, reset] = useForm(
+    import.meta.env.VITE_CONTACT_FORM_ID
+  );
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    if (state.errors) setError(true);
+    if (!error && state.succeeded) setError(false);
+  }, [state, error]);
+
   return (
-    <form className="trialForm">
+    <form className="trialForm" onSubmit={handleSubmit}>
       <div className="titles">
-        <h2>Book a Trial!</h2>
-        <p>Let's meet</p>
+        <h2>Book your Trial today!</h2>
+        <p>
+          Currently available only in{" "}
+          <b>
+            <i>Canada</i>
+          </b>{" "}
+          and the{" "}
+          <b>
+            <i>USA</i>
+          </b>
+          .
+        </p>
       </div>
 
       <div className="inputsContainer">
         <div className="input">
-          <label htmlFor="name">Nombre / Name *</label>
-          <input type="text" id="name" name="name" />
+          <label htmlFor="name">Name *</label>
+          <input type="text" id="name" name="name" required />
+          <ValidationError
+            field="name"
+            errors={state.errors}
+            className="error"
+          />
         </div>
         <div className="input">
           <label htmlFor="email">E-mail *</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" name="email" required />
+          <ValidationError
+            field="email"
+            errors={state.errors}
+            className="error"
+          />
         </div>
         <div className="input">
-          <label htmlFor="phone">Celular / Phone number</label>
+          <label htmlFor="phone">Phone number</label>
           <input type="text" id="phone" name="phone" />
         </div>
         <div className="input">
-          <label htmlFor="country">País / Country *</label>
-          <input type="text" id="country" name="country" />
+          <label htmlFor="country">Country *</label>
+          <select name="country" id="country" required>
+            <option value="Canada">Canada</option>
+            <option value="USA">USA</option>
+          </select>
         </div>
         <div className="input">
-          <label htmlFor="location">Ubicación / Location *</label>
-          <input type="text" id="location" name="location" />
+          <label htmlFor="location">Location *</label>
+          <input type="text" id="location" name="location" required />
+          <ValidationError
+            field="location"
+            errors={state.errors}
+            className="error"
+          />
         </div>
         <div className="input">
-          <label htmlFor="company">Empresa / Company *</label>
-          <input type="text" id="company" name="company" />
+          <label htmlFor="company">Company *</label>
+          <input type="text" id="company" name="company" required />
+          <ValidationError
+            field="company"
+            errors={state.errors}
+            className="error"
+          />
         </div>
         <div className="input">
-          <label htmlFor="numberOfHouses">
-            Número de galpones / Number of houses *
-          </label>
-          <input type="number" id="numberOfHouses" name="numberOfHouses" />
+          <label htmlFor="numberOfHouses">Number of houses *</label>
+          <input
+            type="number"
+            id="numberOfHouses"
+            name="numberOfHouses"
+            required
+          />
+          <ValidationError
+            field="numberOfHouses"
+            errors={state.errors}
+            className="error"
+          />
         </div>
         <div className="dateTimeInputs">
           <div className="input">
             <label htmlFor="dateOfPreference">Date of preference *</label>
-            <input type="date" id="dateOfPreference" name="dateOfPreference" />
+            <input
+              type="date"
+              id="dateOfPreference"
+              name="dateOfPreference"
+              required
+            />
           </div>
           <div className="input">
             <label htmlFor="timeOfPreference">Time of preference *</label>
-            <input type="time" id="timeOfPreference" name="timeOfPreference" />
+            <input
+              type="time"
+              id="timeOfPreference"
+              name="timeOfPreference"
+              required
+            />
           </div>
         </div>
+        <ValidationError
+          field="dateOfPreference"
+          errors={state.errors}
+          className="error"
+        />
+        <ValidationError
+          field="timeOfPreference"
+          errors={state.errors}
+          className="error"
+        />
         <div className="input">
-          <label htmlFor="message">Mensaje / Message *</label>
-          <textarea name="message" id="message" rows={4}></textarea>
+          <label htmlFor="message">Message *</label>
+          <textarea name="message" id="message" rows={4} required></textarea>
+          <ValidationError
+            field="message"
+            errors={state.errors}
+            className="error"
+          />
         </div>
       </div>
-      <button type="submit" className="button button--solid">
-        Send
-      </button>
+
+      <div className="sendButtonContainer">
+        {state.succeeded ? (
+          <>
+            <p>Thank you for your request, we will reply soon.</p>{" "}
+            <button className="button button--outline" onClick={reset}>
+              Send another request
+            </button>
+          </>
+        ) : (
+          <>
+            {error && <p className="error">Ha ocurrido un error.</p>}
+            <button
+              type="submit"
+              className="button button--solid"
+              disabled={state.submitting}
+            >
+              {state.submitting ? "Sending" : "Send"}
+            </button>
+          </>
+        )}
+      </div>
     </form>
   );
 };
