a4421152c192776cbb149944ff18c8286dbe2514
Francisco Cáceres <francaceres98@gmail.com>
2024-12-18 13:36:12 -0300

    refactor(aboutUs page)

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
+// Home
 export { default as heroMobile } from "/assets/picture/heroMobile.jpg";
 export { default as flockerMobile } from "/assets/picture/flockerMobile.jpg";
 export { default as illustration1 } from "/assets/picture/illustration1.png";
@@ -11,6 +12,20 @@ export { default as app } from "/assets/picture/app.png";
 export { default as chickensMobile } from "/assets/picture/chickensMobile.jpg";
 export { default as poultryScience } from "/assets/picture/poultryScience.jpg";
 
+// About us
+export { default as staff1 } from "/assets/picture/staff1.jpg";
+export { default as staff2 } from "/assets/picture/staff2.jpg";
+export { default as staff3 } from "/assets/picture/staff3.jpg";
+export { default as staff4 } from "/assets/picture/staff4.jpg";
+export { default as staff5 } from "/assets/picture/staff5.jpg";
+export { default as supporter1 } from "/assets/picture/supporter1.png";
+export { default as supporter2 } from "/assets/picture/supporter2.png";
+export { default as supporter3 } from "/assets/picture/supporter3.png";
+export { default as supporter4 } from "/assets/picture/supporter4.png";
+export { default as supporter5 } from "/assets/picture/supporter5.png";
+export { default as supporter6 } from "/assets/picture/supporter6.png";
+
+// Logos
 export { default as aviRobotsLogo } from "/assets/logo/aviRobotsLogo.png";
 export { default as apelieIsologo } from "/assets/logo/apelieIsologo.png";
 export { default as apelieLogo } from "/assets/logo/apelieLogo.png";
